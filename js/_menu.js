7c0a40863bcda82a8545b97d0ab71f21aa53027a
Zinah Alqeji <zinah.poulus@gmail.com>
2026-04-15 08:06:53 +0200

    build menu.js

--- js/_menu.js
+++ js/_menu.js
@@ -1,4 +1,14 @@
-createMenu('Statistics Template JS', [
-  { name: 'Kom igång!', script: 'startpage.js' },
-  { name: 'Övningsinstruktioner', script: 'instructions.js' }
+createMenu('Sweden Elections Analysis', [
+  { name: 'Introduction', script: 'intro.js' },
+
+  { name: 'Analysis', sub: [
+    { name: 'Income vs Voting', script: 'income.js' },
+    { name: 'Age vs Voting', script: 'ages.js' },
+    { name: 'Geography', script: 'geo.js' }
+  ]},
+
+  { name: 'Hypothesis Testing', script: 'hypothesis.js' },
+  { name: 'Data Sources', script: 'sources.js' },
+  { name: 'Conclusion', script: 'conclusion.js' },
+  { name: 'Startpage', script: 'startpage.js' }
 ]);
