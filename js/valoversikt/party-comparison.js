f5e87ee3b3ba0b5e8c33708bb0181b2ec618caae
bubul.com <amindalshad42@gmail.com>
2026-05-11 09:23:26 +0200

    klar med overview

--- js/valoversikt/party-comparison.js
+++ js/valoversikt/party-comparison.js
@@ -1,2 +1,5 @@
 import { electionResults } from "../helper/dataLoader.js";
 import dbInfoOk, { displayDbNotOkText } from "../helper/dbInfoOk.js";
+
+//I vilka kommuner/län skedde de största förändringarna?
+//Finns det mönster i var förändringarna sker?
